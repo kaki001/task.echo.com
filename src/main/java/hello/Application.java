9775e7383459e0bf7d914cfdf144419d254c5a22
echo.ge <echo.ge@yingzt.com>
2017-09-29 15:49:37 +0800

    提交一个测试代码

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,28 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+public class Application {
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        return executor;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class);
+    }
+}
